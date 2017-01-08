68c5cdc50d0de520a09c75a6e90efc3137b5c348
Ben Crist <ben@magicmoremagic.com>
2017-01-08 15:52:37 -0600

    build.lua syntax overhaul

--- build.lua
+++ build.lua
@@ -1,69 +1,47 @@
-module { name = 'core',
-   projects = {
-      lib { suffix = 'id',
-         src = {
-            'src/id.cpp',
-            'src/namespaced_id.cpp'
-         },
-         preprocessor = {
-            'BE_CORE_ID_IMPL',
-            'BE_CORE_IMPL'
-         }
+module 'core' {
+   lib '-id' {
+      src 'src/id.cpp',
+      src 'src/namespaced_id.cpp',
+      define 'BE_CORE_ID_IMPL',
+      define 'BE_CORE_IMPL'
+   },
+   lib '-id-with-names' {
+      src 'src/id.cpp',
+      src 'src/namespaced_id.cpp',
+      define {
+         'BE_CORE_ID_IMPL',
+         'BE_CORE_IMPL',
+         'BE_ID_NAMES_ENABLED'
       },
-      lib { suffix = 'id-with-names',
-         src = {
-            'src/id.cpp',
-            'src/namespaced_id.cpp'
-         },
-         preprocessor = {
-            'BE_CORE_ID_IMPL',
-            'BE_CORE_IMPL',
-            'BE_ID_NAMES_ENABLED'
-         },
-         exported_preprocessor = {
-            'BE_ID_NAMES_ENABLED'
-         }
+      export_define 'BE_ID_NAMES_ENABLED'
+   },
+   lib {
+      limp 'include/log_attrib_ids.hpp',
+      limp 'include/service_ids.hpp',
+      src {
+         'src/*.cpp',
+         'src/native/*.cpp',
+         'src/native/$(toolchain)/*.cpp',
+         exclude 'src/id.cpp',
+         exclude 'src/namespaced_id.cpp'
       },
-      lib {
-         limp = {
-            'include/log_attrib_ids.hpp',
-            'include/service_ids.hpp'
-         },
-         src = {
-            'src/*.cpp',
-            'src/native/*.cpp',
-            'src/native/' .. toolchain .. '/*.cpp',
-            pch = 'src/pch.cpp',
-            exclude = {
-               'src/id.cpp',
-               'src/namespaced_id.cpp'
-            }
-         },
-         preprocessor = {
-            'BE_CORE_IMPL'
-         }
-      },
-      app { suffix = 'test',
-         icon = 'icon/bengine-test-perf.ico',
-         src = {
-            'test/*.cpp'
-         },
-         libs = {
-            'testing',
-            'core',
-            'core-id'
-         }
-      },
-      app { suffix = 'perf',
-         icon = 'icon/bengine-test-perf.ico',
-         src = {
-            'perf/*.cpp'
-         },
-         libs = {
-            'testing',
-            'core',
-            'core-id'
-         }
+      pch_src 'src/pch.cpp',
+      define 'BE_CORE_IMPL'
+   },
+   app '-test' {
+      icon 'icon/bengine-test-perf.ico',
+      link_project {
+         'testing',
+         'core',
+         'core-id'
+      }
+   },
+   app '-perf' {
+      icon 'icon/bengine-test-perf.ico',
+      link_project {
+         'testing',
+         'core',
+         'core-id'
       }
    }
 }
